21be935eb6e9d899d5d23265d042b35170b39389
Vladyslav <mushinmao@protonmail.com>
2023-12-13 16:24:24 +0200

    Initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'controller_method' => 'index()',
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'controller_method' => 'about()',
+        ]);
+    }
+}
